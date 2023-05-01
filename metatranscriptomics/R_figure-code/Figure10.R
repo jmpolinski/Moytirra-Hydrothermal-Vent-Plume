49bd123ab5879c2699575ba7b46c6e5d914a994c
nextjen92 <jennifermpolinski@gmail.com>
2023-05-01 14:11:53 -0400

    organizing code

---
+++ metatranscriptomics/R_figure-code/Figure10.R
@@ -0,0 +1,28 @@
+###-------- load packages you need --------###
+library(pheatmap)
+
+###-------- load data --------###
+dta <- read.csv("input/virusde.csv", header=TRUE, row.names = 1)
+sd <- read.csv("input/virussd.csv", header=TRUE, row.names = 1)
+turb <- read.csv("input/turbid.csv", header = TRUE, row.names = 1)
+ann_colors = list(turbidity = c("firebrick", "yellow", "darkgreen"))
+
+###-------- set colors and other formatting --------###
+col <- colorRampPalette(c("dodgerblue","white","red"))(256)
+myBreaks <- c(seq(min(dta), 0, length.out=ceiling(256/2) + 1), 
+              seq(max(dta)/256, max(dta), length.out=floor(256/2)))
+
+
+###-------- generate pheatmap plot --------###
+pheatmap(dta, cluster_cols=FALSE, 
+         breaks = myBreaks,
+         color = col,
+         gaps_col = c(6,8),
+         fontsize_row = 5, 
+         annotation_col=turb, 
+         annotation_colors=ann_colors,
+         show_rownames = FALSE,
+         cellwidth = 9,
+         cellheight = 3.3,
+         display_numbers = matrix(ifelse(sd < 0.05, "*", ""),nrow(sd))
+         )
